b12997ed32b3ef10bcc4bf729c4246db7f04fd17
Wizqqr <baban1979molotov@gmail.com>
2024-04-30 22:26:58 +0600

    FirstSeminar

---
+++ First/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа проверки квадрата
+
+Console.Write("Could you write a number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Could you write second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 == num2 * num2)
+{
+    Console.WriteLine("Your first number is the square of second number");
+}
+else
+{
+    Console.WriteLine("Your first number is not the square of second number");
+}
---
+++ Second/Program.cs
@@ -0,0 +1,42 @@
+﻿// программа -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2
+
+// Console.WriteLine("Type some number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int count = -num;
+
+// while(count <= num)
+// {
+//     Console.Write(count + " ");
+//     count ++;
+//     count += 1;
+// }
+
+
+Console.WriteLine("Type some number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num > 0)
+{
+    int count = -num;
+    
+    while (count <= num)
+    {
+        Console.Write(count + " ");
+        count++;
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
+
+
+
+
+
+
+
